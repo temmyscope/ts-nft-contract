86a21c9f5512226bcb4088d3a7769f4cc7ec73b4
Elisha OYAWALE <elishaoyawale@Elishas-MacBook-Pro.local>
2022-03-25 09:38:49 +0100

    Add Minting Logic

--- contracts/TsToken.sol
+++ contracts/TsToken.sol
@@ -2,12 +2,34 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract TsToken {
+contract TsToken is ERC721URIStorage {
 
-    constructor() {
+    using Counters for Counters.Counter;
+    Counters.Counter private __tokenId;
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("TemmyScope NFT contract");
+    }
+
+    //NFT minter
+    function mintNFTbyTS() public {
+        uint256 newItemId = __tokenId.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/SCA4");
+
+        console.log(
+            "An NFT w/ ID %s has been minted to %s", newItemId, msg.sender
+        );
+
+        // Increment the counter for when the next NFT is minted.
+        __tokenId.increment();
     }
 
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,8 +5,12 @@ async function main() {
   const nftContractFactory = await ethers.getContractFactory('TsToken');
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
-
   console.log("Contract deployed to:", nftContract.address);
+
+  //start minting
+  let txn = await nftContract.mintNFTbyTS()
+  // Wait for it to be mined.
+  await txn.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
